f79b6a0d9301e1336317fe7f7e3494932c0a108c
traoremp <traoremp@vt.edu>
2018-01-20 13:33:26 -0500

    TO-DO LIST

--- main_program/src/main.rs
+++ main_program/src/main.rs
@@ -9,5 +9,10 @@ use std::env;
 
 fn main(){
     println!("Hello World");
+    /*TO-DO LIST
+        * Open three types of file: .mp4, .mp3, .txt
+        * Convert to binary
+        * encode - decode
+    */
 
 }
