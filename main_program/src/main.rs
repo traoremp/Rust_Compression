269254c8b3843844bf87f765d0686b618d1183c1
Ala-eddine <a-e.kamate@hotmail.com>
2018-02-01 23:20:57 -0500

    serious advancements

--- main_program/src/main.rs
+++ main_program/src/main.rs
@@ -18,19 +18,25 @@ use lzw::{LsbWriter, Encoder};
 use cpuprofiler::PROFILER;
 // use image::DynamicImage;
 
-fn encode_lzw( compressed : &mut Vec<u8>, dict_size : u8, data_to_encode : &[u8]){
-    
+fn encode_lzw( compressed : &mut Vec<u8>, dict_size : u8, data_to_encode : &[u8], file_name: &str){
+
+    PROFILER.lock().unwrap().start(file_name.to_string() + "-encoding.profile").expect("Couldn't start");
     let mut encoder = Encoder::new(LsbWriter::new(compressed), dict_size).unwrap();
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop"); 
+
+    PROFILER.lock().unwrap().start(file_name.to_string() + "-compressing.profile").expect("Couldn't start");
     encoder.encode_bytes(data_to_encode).unwrap();
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop"); 
 }
 
 fn main(){
+    
     /*TO-DO LIST
         * Open three types of file: .mp4, .mp3, .txt
         * Convert to binary
         * encode - decode
     */
-    let file_name = "Addison.Wesley.Effective.CPP.3rd.Edition.May.2005.pdf";
+    let file_name = "docs_lab_1_techno_multimedia/long_text2.txt";
     let mut file = match File::open(file_name){
 
         Ok(file) => file,
@@ -43,7 +49,7 @@ fn main(){
     // let dict_size = 8;
     let mut compressed : Vec<u8> = vec!();
     {
-        encode_lzw(&mut compressed, 15, &my_string_buffer[..] );
+        encode_lzw(&mut compressed, 8, &my_string_buffer[..], "lzw_text");
     }
     // println!("{}", my_string_buffer.len());
     // println!("{}", compressed.len() );
@@ -58,9 +64,13 @@ fn main(){
     let substrings = string_to_substrings(&my_string_buffer[..], 1);
     //println!("{:?}", my_string_content);
     //println!("{:?}", substrings);
+    PROFILER.lock().unwrap().start("huffman_txt".to_string() + "-encoding.profile").expect("Couldn't start");
     let codebook = Codebook::new(&substrings);
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop"); 
     //println!("{:?}", codebook.character_map);
+     PROFILER.lock().unwrap().start("huffman_txt".to_string() + "-compressing.profile").expect("Couldn't start");
     let compression_results = parallel_compress(&substrings, &codebook);
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop");
     // print_summary(compression_results, my_string_content.len());
     let compressed_size_huffman = compression_results.iter().fold(0, |acc, ref result|  acc + result.bytes.len());
     let compression_rate_huffman = compressed_size_huffman as f32 / my_string_buffer.len() as f32;
@@ -68,15 +78,13 @@ fn main(){
     println!("\nHuffman Compressed bytes size {:?}, from {:?}. Ratio: {:?}, Rate {:?}", compressed_size_huffman, my_string_buffer.len(), compression_ratio_huffman, compression_rate_huffman);
 
     //IMAGE 
-    let my_image = image::open("huffman_david.99-10-11.bmp").unwrap();
+    let my_image = image::open("docs_lab_1_techno_multimedia/a80f04d3ac7b69dbf5b3b1fc8dd24489.jpg").unwrap();
     let my_rgb_image = my_image.to_rgb();
     let my_image_as_vec = my_rgb_image.into_raw();
     let mut compressed : Vec<u8> = vec!();
     // LZW image encoding
     {
-        PROFILER.lock().unwrap().start("./my-prof.profile").expect("Couldn't start");
-        encode_lzw(&mut compressed, 8, &my_image_as_vec[..] ); 
-        PROFILER.lock().unwrap().stop().expect("Couldn't stop");        
+        encode_lzw(&mut compressed, 12, &my_image_as_vec[..], "lzw_image" ); 
     }
     
     let compression_rate = compressed.len() as f32 / my_image_as_vec.len() as f32 ;
@@ -90,12 +98,17 @@ fn main(){
     let substrings = string_to_substrings(&my_image_as_vec[..], 1);
     //println!("{:?}", my_string_content);
     //println!("{:?}", substrings);
+    PROFILER.lock().unwrap().start("huffman_image".to_string() + "-encoding.profile").expect("Couldn't start");
     let codebook = Codebook::new(&substrings);
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop"); 
     //println!("{:?}", codebook.character_map);
+    PROFILER.lock().unwrap().start("huffman_image".to_string() + "-compressing.profile").expect("Couldn't start");
     let compression_results = parallel_compress(&substrings, &codebook);
+    PROFILER.lock().unwrap().stop().expect("Couldn't stop"); 
     //print_summary(compression_results, my_string_content.len());
      let compressed_size_huffman = compression_results.iter().fold(0, |acc, ref result|  acc + result.bytes.len());
     let compression_rate_huffman = compressed_size_huffman as f32 / my_image_as_vec.len() as f32;
     let compression_ratio_huffman =   my_image_as_vec.len() as f32/  compressed_size_huffman as f32;
     println!("\nHuffman Compressed bytes size {:?}, from {:?}. Ratio: {:?}, Rate {:?}\n", compressed_size_huffman, my_image_as_vec.len(), compression_ratio_huffman, compression_rate_huffman);
+         
 }
